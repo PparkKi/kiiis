5dff97372c01e3cd20c53caa020ec7b46abac5f3
PparkKi <fkawl213@gmail.com>
2022-07-17 18:27:19 +0900

    update account

--- src/routes/Account.js
+++ src/routes/Account.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { authService, googleProvider, facebookProvider } from "fbase";
 import { signInWithPopup } from "firebase/auth";
 import AccForm from "components/AccForm";
+
+import Modal from "@mui/material/Modal";
+import Box from "@mui/material/Box";
+
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook } from "react-icons/fa";
+
+const style = {
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
+  minWidth: 350,
+  bgcolor: "background.paper",
+  border: "1px solid #7b517f",
+  boxShadow: 24,
+  p: 4,
+};
 
 const Account = () => {
   const params = useParams();
   const navigate = useNavigate();
+
+  const [open, setOpen] = useState(true);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
   let accVal = Boolean;
   if (params.accountType === "login") accVal = true;
@@ -56,6 +76,25 @@ const Account = () => {
           <FaFacebook size={20} className="mr-05" /> facebook 로그인
         </button>
       </div>
+
+      <Modal
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style}>
+          우측 상단 "회원가입" 버튼 클릭 시 회원가입이 가능하며
+          <br />
+          테스트 계정을 사용하셔도 됩니다!
+          <br /><br />
+          <p>
+            ID : t1@t1.com
+            <br />
+            PW : asd123
+          </p>
+        </Box>
+      </Modal>
     </div>
   );
 };
