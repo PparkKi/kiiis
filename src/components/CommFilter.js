8cb01648e978700c740084ba7d8551612932778d
PparkKi <fkawl213@gmail.com>
2022-07-17 15:26:09 +0900

    edit styled 2

--- src/components/CommFilter.js
+++ src/components/CommFilter.js
@@ -10,28 +10,28 @@ import Select from "@mui/material/Select";
 import { styled } from "@mui/material/styles";
 import { HiOutlineSearch } from "react-icons/hi";
 
-const ColorSelect = styled(Select)({
-  "& .css-1d3z3hw-MuiOutlinedInput-notchedOutline": {
-    border: "none",
-  },
-});
-const ColorTextField = styled(TextField)({
-  "& .css-9ddj71-MuiInputBase-root-MuiOutlinedInput-root": {
-    borderRadius: "30px",
-    backgroundColor: "#fff",
-  },
-  "& .css-1d3z3hw-MuiOutlinedInput-notchedOutline": {
-    border: "none",
-    boxShadow:
-      "0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%) !important",
-  },
-});
-
 const CommFilter = ({ cafeAllData }) => {
   const dispatch = useDispatch();
   const [keyword, setKeyword] = useState("");
   const [searchCafe, setSearchCafe] = useState(cafeAllData);
   const [selected, setSelected] = useState(true);
+
+  const ColorSelect = styled(Select)({
+    "& .css-1d3z3hw-MuiOutlinedInput-notchedOutline": {
+      border: "none",
+    },
+  });
+  const ColorTextField = styled(TextField)({
+    "& .css-9ddj71-MuiInputBase-root-MuiOutlinedInput-root": {
+      borderRadius: "30px",
+      backgroundColor: "#fff",
+    },
+    "& .css-1d3z3hw-MuiOutlinedInput-notchedOutline": {
+      border: "none",
+      boxShadow:
+        "0px 3px 1px -2px rgb(0 0 0 / 20%), 0px 2px 2px 0px rgb(0 0 0 / 14%), 0px 1px 5px 0px rgb(0 0 0 / 12%)",
+    },
+  });
 
   const isMobile = useMediaQuery({
     query: "(min-width:480px)",
--- src/routes/Intro.js
+++ src/routes/Intro.js
@@ -11,19 +11,19 @@ import Tab from "@mui/material/Tab";
 import Box from "@mui/material/Box";
 import { styled } from "@mui/material/styles";
 
-const ColorTabs = styled(Tabs)({
-  div: {
-    "& .css-1h9z7r5-MuiButtonBase-root-MuiTab-root.Mui-selected": {
-      color: "#7b517f",
-    },
-    "& .css-1aquho2-MuiTabs-indicator": {
-      backgroundColor: "#7b517f",
-    },
-  },
-});
-
 function TabPanel(props) {
   const { children, value, index, ...other } = props;
+
+  const ColorTabs = styled(Tabs)({
+    div: {
+      "& .css-1h9z7r5-MuiButtonBase-root-MuiTab-root.Mui-selected": {
+        color: "#7b517f",
+      },
+      "& .css-1aquho2-MuiTabs-indicator": {
+        backgroundColor: "#7b517f",
+      },
+    },
+  });
 
   return (
     <div
